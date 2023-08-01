526dd69ef73b1e1f96be6f3723637c1694ab8d27
Khan Mohammed <mkhan@ags.wan>
2023-08-01 16:44:30 +0200

    add flex in list

--- src/List.js
+++ src/List.js
@@ -1,4 +1,6 @@
 import React from "react";
+import './List.scss';
+
 
 class List extends React.Component {
     render() {
@@ -6,11 +8,13 @@ class List extends React.Component {
             <div>
                 {this.props.allTodo && this.props.allTodo.map(elm => {
                     return(
-                        <div>
+                        <div className="listItem">
+                            <div className="inputContainer">
                             <input type="checkbox" onClick={()=> this.props.completeTodo(elm)}/>
                             <p style={{textDecoration: elm.isCompleted? "line-through": "none"}}
                                 onClick={() => {this.props.editTodo(elm)}}
                             >{elm.inputText}</p>
+                            </div>
                             <p style={{cursor: 'pointer'}}
                                 onClick={() => this.props.deleteTodo(elm)}
                             >x</p>
